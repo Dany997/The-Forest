3e6460878e508ee87b1e04b08e951da79217d4a1
Dhuzio <dany97@o2.pl>
2024-09-18 15:55:55 +0200

    first

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 	const toggleButton = document.querySelector('.navbar__toggle');
 	const menu = document.querySelector('.navbar__menu');
 	const body = document.body;
-  
-	toggleButton.addEventListener('click', function () {
-	  menu.classList.toggle('navbar--active__menu'); // Pokazuje menu
-	  body.classList.toggle('navbar--active'); // Zablokowanie przewijania strony
-	});
-  });
+
+	const handleNav = () => {
+		menu.classList.toggle('navbar--active__menu'); // Pokazuje menu
+		body.classList.toggle('navbar--active'); // Zablokowanie przewijania strony
+	};
+
+	toggleButton.addEventListener('click', handleNav);
+});
