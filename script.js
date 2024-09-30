af8660ff18a534747be6c15d4b2438f564646285
Dhuzio <dany97@o2.pl>
2024-09-30 15:03:46 +0200

    fixed footer

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	toggleButton.addEventListener('click', handleNav);
 });
+
+window.addEventListener('scroll', function () {
+	let sections = document.querySelectorAll('section');
+	let navLinks = document.querySelectorAll('.navbar__link');
+
+	let scrollY = window.pageYOffset; // Pozycja przewinięcia
+
+	sections.forEach((section) => {
+		let sectionHeight = section.offsetHeight;
+		let sectionTop = section.offsetTop - 50; // Uwzględnia wysokość menu
+		let sectionId = section.getAttribute('id');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			navLinks.forEach((link) => {
+				link.classList.remove('active');
+				if (link.getAttribute('href').includes(sectionId)) {
+					link.classList.add('active');
+				}
+			});
+		}
+	});
+});
+
+
+
+
