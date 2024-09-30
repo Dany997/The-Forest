d8b2a7b42f6bf811e44b0db5872f4dfb2afcfbb4
Dhuzio <dany97@o2.pl>
2024-09-30 22:32:24 +0200

    fixing nav1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const toggleButton = document.querySelector('.navbar__toggle');
 	const menu = document.querySelector('.navbar__menu');
 	const body = document.body;
+	const navbar = document.querySelector('.navbar');
 
 	const handleNav = () => {
 		menu.classList.toggle('navbar--active__menu'); // Pokazuje menu
@@ -9,30 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
 	};
 
 	toggleButton.addEventListener('click', handleNav);
-});
 
-window.addEventListener('scroll', function () {
-	let sections = document.querySelectorAll('section');
-	let navLinks = document.querySelectorAll('.navbar__link');
+	// Dodanie obsługi "scroll" dla sticky effect
+	window.addEventListener('scroll', function () {
+		let scrollY = window.pageYOffset;
 
-	let scrollY = window.pageYOffset; // Pozycja przewinięcia
-
-	sections.forEach((section) => {
-		let sectionHeight = section.offsetHeight;
-		let sectionTop = section.offsetTop - 50; // Uwzględnia wysokość menu
-		let sectionId = section.getAttribute('id');
-
-		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-			navLinks.forEach((link) => {
-				link.classList.remove('active');
-				if (link.getAttribute('href').includes(sectionId)) {
-					link.classList.add('active');
-				}
-			});
+		if (scrollY > 100) {
+			navbar.classList.add('navbar--scrolled');
+		} else {
+			navbar.classList.remove('navbar--scrolled');
 		}
+
+		// Obsługa aktywnych linków nawigacyjnych
+		let sections = document.querySelectorAll('section');
+		let navLinks = document.querySelectorAll('.navbar__link');
+
+		sections.forEach((section) => {
+			let sectionHeight = section.offsetHeight;
+			let sectionTop = section.offsetTop - 50; // Uwzględnia wysokość menu
+			let sectionId = section.getAttribute('id');
+
+			if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+				navLinks.forEach((link) => {
+					link.classList.remove('active');
+					if (link.getAttribute('href').includes(sectionId)) {
+						link.classList.add('active');
+					}
+				});
+			}
+		});
 	});
 });
-
-
-
-
